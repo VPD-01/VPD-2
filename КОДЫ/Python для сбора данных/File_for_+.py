8c5e4ccec8240ae7142f930be6e03e7d452eb441
VPD-01 <sibus1so@yandex.com>
2025-04-21 09:01:46 +0300

    Add files via upload

---
+++ КОДЫ/Python для сбора данных/File_for_+.py
@@ -0,0 +1,22 @@
+from ev3dev2.motor import OUTPUT_B, LargeMotor
+import time
+
+motor = LargeMotor(OUTPUT_B)
+
+for volt in range(10, 56, 5):
+    volt = +volt
+    start_time = time.time()
+    startPos = motor.position
+    filename = "data_" + str(volt) + '.txt'
+    file = open(filename, 'w')
+    while time.time() - start_time < 1:
+        delta_t = time.time() - start_time
+        motor.run_direct(duty_cycle_sp=volt)
+        pos = motor.position - startPos
+        vel = motor.speed
+        file.write(str(time.time()) + ", " + str(pos) + ", " + str(vel) + "\n")
+    else:
+
+        motor.stop()
+        time.sleep(2)
+    file.close()
